8969267676023c6a18d1365f95c67288f7285753
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-30 10:26:31 +0900

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDUTTCHjTaMib1an0NZY4LeScfQwJYyzz4",
+  authDomain: "whatsapp-clone-40cda.firebaseapp.com",
+  projectId: "whatsapp-clone-40cda",
+  storageBucket: "whatsapp-clone-40cda.appspot.com",
+  messagingSenderId: "314678521881",
+  appId: "1:314678521881:web:96da7a3ee8f9d29723ba88",
+  measurementId: "G-PMBDL46M9L"
+};
