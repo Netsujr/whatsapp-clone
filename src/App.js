48bf74cba00298368b39475b5cda115f57f94887
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-31 15:23:57 +0900

    App.js refactored

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 // import logo from './logo.svg';
 import React from 'react';
-import './App.css';
 import Sidebar from './Sidebar';
+import styled from 'styled-components';
+import './App.css';
 
 
 function App() {
   return (
     <div className="app">
       <h1>Renato's WhatsApp Chat</h1>
-      <div className="app_body">
+      <AppBody>
         <Sidebar />
+
         {/* chat area */}
-      </div>
+      </AppBody>
     </div>
   );
 }
 
 export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  width: 90vw;
+  height: 90vh;
+  background-color: #ededed;
+  box-shadow: -1px 4px 20px -6px rgba(0, 0, 0, 0.7);
+`;
