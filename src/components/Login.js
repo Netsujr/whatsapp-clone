9e69253b01352ea14a0813599d54b5f346449b16
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-04 19:20:44 +0900

    google auth works
    settings needed to be changed on firebase. Need to capture info and dispatch next

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,15 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
 import styled from 'styled-components';
+import { auth, provider } from '../firebase';
 
 
 const Login = () => {
 
   const signIn = () => {
-
+    auth.signInWithPopup(provider).then(result =>
+      console.log(result)
+      ).catch(error => alert(error.message));
   }
 
   return (
