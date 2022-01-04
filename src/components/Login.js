8d1fb241ee7daaca1f3caada4ae36c7daa4042f9
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-04 19:11:32 +0900

    login page styled, auth next

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,9 +5,9 @@ import styled from 'styled-components';
 
 const Login = () => {
 
-const signIn = () => {
+  const signIn = () => {
 
-}
+  }
 
   return (
     <LoginContainer>
@@ -17,7 +17,7 @@ const signIn = () => {
           <h1>Sign in to Whatsapp</h1>
         </LoginText>
         <Button onClick={signIn}>
-          Sign In With Google!
+          Sign In With Google
         </Button>
       </InnerContainer>
     </LoginContainer>
@@ -27,11 +27,33 @@ const signIn = () => {
 export default Login;
 
 const LoginContainer = styled.div`
+  background-color: #f8f8f8;
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  place-items: center;
 
   `;
 
 const InnerContainer = styled.div`
+  padding: 100px;
+  text-align: center;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 5px rgba(0, 0, 0, 0.12);
 
+  img {
+    object-fit: contain;
+    height: 100px;
+    margin-bottom: 40px;
+  }
+
+  Button {
+    margin-top: 35px;
+    text-transform: inherit !important;
+    background-color: #0a8d48 !important;
+    color: white;
+  }
   `;
 
 const LoginText = styled.div`
